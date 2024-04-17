9215745d9aafb605ae6fb4b0b11acef3f1a19270
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-04-16 22:22:35 -0400

    Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -1,6 +1,9 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
+
+  PImage imgGooglyEyes;
 
   public void settings() {
     size(400, 400);
@@ -8,10 +11,13 @@ public class Sketch extends PApplet {
 
   public void setup() {
     background(210, 255, 173);
+    // load googly eyes
+    imgGooglyEyes = loadImage ("googlyeyes.png");
   }
 
   public void draw() {
-    
+    // draw googly eyes
+    image (imgGooglyEyes, 0, 0);
   }
 
 }
